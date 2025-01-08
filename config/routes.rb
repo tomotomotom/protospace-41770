5b53a5c95a55fc581d29c046961562c017866385
tomotomotom <torimorinorikeyhome@gmail.com>
2025-01-08 20:57:50 +0900

    各ページのユーザー名をクリックすると、ユーザー詳細ページに遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create,:show,:edit,:update,:destroy]
+  resources :prototypes
+  resources :users , only: :show
   resources :prototypes do
     resources :comments, only: [:create, :destroy ]
   end
