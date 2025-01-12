e81387309f9af4c659b103f5b05d30f96a658700
tomotomotom <torimorinorikeyhome@gmail.com>
2025-01-12 12:32:50 +0900

    画像のばリテーション修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -6,7 +6,7 @@ class Prototype < ApplicationRecord
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
-  validates :content, presence: true ,unless: :was_attached?
+  validates :image, presence: true ,unless: :was_attached?
 
   def was_attached?
     self.image.attached?
